dbc20fac093438b6b51bf9cd3f4039f101a09fa2
zanezgame <zanezgame@gmail.com>
2018-10-22 11:36:38 +0800

    mongodb修改

--- service/mongodb.lua
+++ service/mongodb.lua
@@ -1,5 +1,4 @@
 local skynet = require "skynet"
-local bsonlib = require "bson"
 local mongo = require "skynet.db.mongo"
 require "skynet.manager"
 
@@ -20,63 +19,70 @@ function CMD.init()
     db_client:getDB(db_name)
     db = db_client[db_name]
 
-    skynet.error("connect mongodb succ")
-
-
 end
+
+--selector ：可选，使用查询操作符指定查询条件
+--field_selector ：可选，使用投影操作符指定返回的键。查询时返回文档中所有键值， 只需省略该参数即可（默认省略）。
 
 function CMD.findOne(cname, selector, field_selector)
     return db[cname]:findOne(selector, field_selector)
 end
 
+--selector ：可选，使用查询操作符指定查询条件
+--field_selector ：可选，使用投影操作符指定返回的键。查询时返回文档中所有键值， 只需省略该参数即可（默认省略）。
+
 function CMD.find(cname, selector, field_selector)
-    return db[cname]:find(selector, field_selector)
-end
+    local cursor = db[cname]:find(selector, field_selector)
 
-function CMD.update(cname, ...)
-    local collection = db[cname]
-    collection:update(...)
-    local r = db:runCommand("getLastError")
-    if r.err ~= bsonlib.null then
-        return false, r.err
-    end
-
-    if r.n <= 0 then
-        skynet.error("mongodb update " .. cname .. " failed")
-    end
-
-    return ok, r.err
-end
-
-local ops = { 'insert', 'batch_insert', 'delete' }
-for _, v in ipairs(ops) do
-    CMD[v] = function(self, cname, ...)
-        local c = db[cname]
-        c[v](c, ...)
-        local r = db:runCommand('getLastError')
-        local ok = r and r.ok == 1 and r.err == Bson.null
-        if not ok then
-            skynet.error(v .. " failed: ", r.err, tname, ...)
+    if cursor:hasNext() then
+        local results = {};
+        local count = cursor:count()
+        for i = 1, count, 1 do
+            table.insert(results, cursor:next())
         end
-        return ok, r.err
+        return results
+    else
+        return nil
     end
+end
+
+function CMD.insert(cname, doc)
+    return db[cname]:safe_insert(doc)
+end
+
+--selector :（可选）删除的文档的条件。
+--single : （可选）如果设为 true 或 1，则只删除一个文档
+
+function CMD.delete(cname, selector, single)
+    return db[cname]:safe_delete(selector, single)
+end
+
+--query : update的查询条件，类似sql update查询内where后面的。
+--update : update的对象和一些更新的操作符（如$,$inc...）等，也可以理解为sql update查询内set后面的
+--upsert : 可选，这个参数的意思是，如果不存在update的记录，是否插入objNew,true为插入，默认是false，不插入。
+--multi : 可选，mongodb 默认是false,只更新找到的第一条记录，如果这个参数为true,就把按条件查出来多条记录全部更新。
+
+function CMD.update(cname, selector, update, upsert, multi)
+    return db[cname]:safe_update(selector, update, upsert, multi)
+end
+
+local dispatch = function(session, addr, command, ...)
+    local f = CMD[command]
+    assert(f, "Invalid command")
+    local ok, ret = pcall(f, ...)
+    skynet.ret(skynet.pack(ret))
 end
 
 skynet.start(function()
 
-    skynet.dispatch("lua", function(session, addr, command, ...)
-
-        local f = CMD[command]
-        assert(f, "Invalid command")
-
-        local ok, ret = pcall(f, ...)
-        if ok then
-            skynet.ret(skynet.pcak(ret))
-        end
-    end)
+    skynet.dispatch("lua", dispatch)
 
     skynet.register("mongodb")
 
     CMD.init()
+
+    --local data = skynet.call(skynet.self(), "lua", "update", "user", { user = 1 }, { ["$set"] = { user = 100, name = "zane1" } }, false, true)
+    --local data = skynet.call(skynet.self(), "lua", "insert", "user", { user = 1000 })
+    --skynet.error(data)
 
 end)
